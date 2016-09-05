0d7d29d58f8bb32ad9ee7a86cee1c2b3a529d071
kenjinote <kenji256@gmail.com>
2016-09-05 18:40:33 +0900

    add files.

---
+++ Source.cpp
@@ -0,0 +1,111 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+
+TCHAR szClassName[] = TEXT("Window");
+#define N 10
+
+void BubleSort(int* sort, int nCount)
+{
+	BOOL bFlag;
+	int k = 0;
+	do {
+		bFlag = FALSE;
+		for (int i = 0; i < nCount - 1 - k; i++)
+		{
+			if (sort[i] > sort[i + 1])
+			{
+				bFlag = TRUE;
+				const int nTmp = sort[i];
+				sort[i] = sort[i + 1];
+				sort[i + 1] = nTmp;
+			}
+		}
+		k++;
+	} while (bFlag);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static int sort[N];
+	static HWND hButton;
+	switch (msg)
+	{
+	case WM_CREATE:
+		hButton = CreateWindow(TEXT("BUTTON"), TEXT("ソート"), WS_VISIBLE | WS_CHILD, 0, 0, 0, 0, hWnd, (HMENU)IDOK, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+		for (int i = 0; i < N; i++)
+		{
+			sort[i] = rand() % N;
+		}
+		break;
+	case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+			TCHAR szText[256];
+			for (int i = 0; i < N; i++)
+			{
+				wsprintf(szText, TEXT("%d"), sort[i]);
+				TextOut(hdc, 10, i * 32 + 50, szText, lstrlen(szText));
+			}		
+			EndPaint(hWnd, &ps);
+		}
+		break;
+	case WM_SIZE:
+		MoveWindow(hButton, 10, 10, 256, 32, TRUE);
+		break;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK)
+		{
+			BubleSort(sort, N);
+			InvalidateRect(hWnd, 0, 1);
+			MessageBox(hWnd, TEXT("ソートしました"), TEXT("確認"), 0);
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	MSG msg;
+	WNDCLASS wndclass = {
+		CS_HREDRAW | CS_VREDRAW,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0,IDC_ARROW),
+		(HBRUSH)(COLOR_WINDOW + 1),
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("バブルソート"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
